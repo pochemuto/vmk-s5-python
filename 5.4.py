d750025b2be204a7d18705161f4c3042e57c76f2
pochemuto <pochemuto@pochemuto-mac.local>
2014-11-06 20:54:22 +0300

    5.4 ok

--- 5.4.py
+++ 5.4.py
@@ -6,9 +6,9 @@ a = list(a) if type(a) is tuple else [a]
 s = input()
 for i,n in enumerate(a):
   if i < s:
-    a[i] = (n, [0])
+    a[i] = n
   else:
     prev_jump = min(a[i-s:i])
-    a[i] = (n + prev_jump[0], prev_jump[1] + [i])
+    a[i] = n + prev_jump
 
-print a[-1][0]
+print a[-1]
